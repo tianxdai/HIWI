c47605c4fefe68c9e74876fd0ce69180aba4e1d7
tianxdai <tianxdai@outlook.com>
2020-03-20 17:28:08 +0100

    add folder

---
+++ trajectory_tracking/aruco_tracking_2D.py
@@ -0,0 +1,112 @@
+# USAGE
+# python3 aruco_tracking_2D.py --video aruco_video_single.mp4
+
+# import the necessary packages
+from collections import deque
+from imutils.video import VideoStream
+import numpy as np
+import argparse
+from cv2 import aruco
+import cv2
+import imutils
+import time
+
+
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-v", "--video",
+	help="path to the (optional) video file")
+ap.add_argument("-b", "--buffer", type=int, default=64,
+	help="max buffer size")
+args = vars(ap.parse_args())
+
+# define the lower and upper boundaries of the "green"
+# ball in the HSV color space, then initialize thee
+# list of tracked points
+yellowLower = (30/360*255, 50*2.55, 30*2.55)
+yellowUpper = (45/360*255,100*2.55,100*2.55)
+greenLower = (70/360*255, 50*2.55, 30*2.55)
+greenUpper = (120/360*255, 100*2.55, 100*2.55)
+blueLower = (230/360*255, 50*2.55, 30*2.55)
+blueUpper = (250/360*255, 100*2.55, 100*2.55)
+pts_g = deque(maxlen=args["buffer"])
+pts_y = deque(maxlen=args["buffer"])
+pts_b = deque(maxlen=args["buffer"])
+n_color = 2
+colorLowers = (yellowLower,greenLower,blueLower)
+colorUppers = (yellowUpper,greenUpper,blueUpper)
+pts_color = (pts_y,pts_g,pts_b)
+
+# if a video path was not supplied, grab the reference
+# to the webcam
+if not args.get("video", False):
+	vs = VideoStream(src=0).start()
+
+# otherwise, grab a reference to the video file
+else:
+	vs = cv2.VideoCapture(args["video"])
+
+# allow the camera or video file to warm up
+time.sleep(2.0)
+
+f_name = args.get('video').replace('.mp4','_tracking.txt')
+f_out = open(f_name, 'w')
+
+# keep looping
+while True:
+	current_time = vs.get(cv2.CAP_PROP_POS_MSEC)/1000
+	
+	# grab the current frame
+	frame = vs.read()
+
+	# handle the frame from VideoCapture or VideoStream
+	frame = frame[1] if args.get("video", False) else frame
+
+	# if we are viewing a video and we did not grab a frame,
+	# then we have reached the end of the video
+	if frame is None:
+		break
+
+	# rotate the current frame
+	#frame = imutils.rotate(frame, -90)
+
+	# resize the frame, blur it, and convert it to the HSV
+	# color space
+	frame = imutils.resize(frame, height=800)
+	#blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+	aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
+	parameters =  aruco.DetectorParameters_create()
+	corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
+	frame_markers = aruco.drawDetectedMarkers(frame.copy(), corners, ids)
+	
+	if corners:
+		x = corners[0][0][:,0].mean()
+		y = corners[0][0][:,1].mean()
+		cv2.circle(frame_markers, (int(x), int(y)), int(5),
+					(0, 255, 255), 2)
+		#print('===========================')
+		#print(corners[0][0])
+		
+	# show the frame to our screen
+	cv2.imshow("Frame", frame_markers)
+	key = cv2.waitKey(1) & 0xFF
+
+	# if the 'q' key is pressed, stop the loop
+	if key == ord("q"):
+		break
+
+f_out.close()
+
+# if we are not using a video file, stop the camera video stream
+if not args.get("video", False):
+	vs.stop()
+
+# otherwise, release the camera
+else:
+	vs.release()
+
+# close all windows
+cv2.destroyAllWindows()
